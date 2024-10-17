2183493d441aa0b0cc87e2dbeec548dd07487771
Indy Iri <indy.iri@student.pxl.be>
2024-10-17 14:48:52 +0200

    Bijna afgewerkt: uitkomst newPopulation komt niet volledig uit

--- H3Oef11Bevolkingsgroei/H3Oef11Bevolkingsgroei/MainWindow.xaml.cs
+++ H3Oef11Bevolkingsgroei/H3Oef11Bevolkingsgroei/MainWindow.xaml.cs
@@ -32,11 +32,40 @@ namespace H3Oef11Bevolkingsgroei
             growthPercentageTextBox.Clear();
             currentPopulationTextBox.Clear();
             resultTextBox.Clear();
+            countryTextBox.Focus();
         }
 
         private void calculateButton_Click(object sender, RoutedEventArgs e)
         {
+            string country = countryTextBox.Text;
+            double currentPopulation;
+            double growthPercentage;
+            string inputCurrentPopulation = currentPopulationTextBox.Text;
+            string inputGrowthPercentage = growthPercentageTextBox.Text;   
 
+            bool isInputCurrentPopulationValid = double.TryParse(inputCurrentPopulation, out currentPopulation);
+            bool isInputGrowthPercentageValid = double.TryParse(inputGrowthPercentage, out growthPercentage);
+
+            double newPopulation = 2.00 * currentPopulation;
+            double yearsToDouble = 0;
+
+            if (isInputCurrentPopulationValid && isInputGrowthPercentageValid && growthPercentage != 0)
+            {
+                while (currentPopulation <= newPopulation)
+                {
+                    currentPopulation *= ((growthPercentage / 100.00) + 1.00);
+                    yearsToDouble++;
+                }
+                resultTextBox.Text = $"Verdubbeling bevolking van {country} na {yearsToDouble} jaar.\n\rNieuwe bevolkingsaantal op dat moment: {newPopulation}";
+            }           
+            else if ((!isInputCurrentPopulationValid || !isInputGrowthPercentageValid) && growthPercentage != 0)
+            {
+                resultTextBox.Text = "Geef een correcte huidige bevolking en groeipercentage in";
+            }
+            else
+            {
+                resultTextBox.Text = "Groeipercentage mag niet 0 zijn: zonder groeipercentage nooit een verdubbeling van de bevolking";
+            }
         }
     }
 }
